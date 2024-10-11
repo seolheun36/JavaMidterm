fb9b9d9a7cbffd4410713763cc98f5b18190c343
seolheun5 <piberius5@gmail.com>
2024-10-11 22:05:15 +0900

    계산기 프레임 기본 설정 및 메인 클래스와 구분

---
+++ src/exam/seolheun5/calculator/main/Calculator.java
@@ -0,0 +1,35 @@
+package exam.seolheun5.calculator.main;
+
+import exam.seolheun5.calculator.ui.CalculatorFrame;
+
+/**
+ * 메인 클래스
+ *
+ * @author seolheun5(김은성, piberius5@gmail.com)
+ *
+ * @created 2024-10-11
+ * @lastModified 2024-10-11
+ *
+ * @changelog
+ * <ul>
+ *     <li>2024-10-11: 최초 생성 (seolheun5)</li>
+ * </ul>
+ */
+public class Calculator {
+    /**
+ * 메인 메서드
+ *
+ * @author seolheun5(김은성, piberius5@gmail.com)
+ *
+ * @created 2024-10-11
+ * @lastModified 2024-10-11
+ *
+ * @changelog
+ * <ul>
+ *     <li>2024-10-11: 최초 생성 (seolheun5)</li>
+ * </ul>
+ */
+    public static void main(String[] args) {
+        CalculatorFrame frame = new CalculatorFrame();
+    }
+}
---
+++ src/exam/seolheun5/calculator/ui/CalculatorFrame.java
@@ -0,0 +1,14 @@
+package exam.seolheun5.calculator.ui;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class CalculatorFrame extends JFrame {
+    public CalculatorFrame() {
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(335, 540);
+        setVisible(true);
+    }
+}
