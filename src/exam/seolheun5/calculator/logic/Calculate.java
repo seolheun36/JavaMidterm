b3eaf3ca9875e59a179372c009c1bbe37f745f62
seolheun5 <piberius5@gmail.com>
2024-10-27 03:39:13 +0900

    fix: Calculate result 오류 수정
    - Calculate 클래스에서 result가 한 자리일 때 발생하던 오류 예외처리

--- src/exam/seolheun5/calculator/logic/Calculate.java
+++ src/exam/seolheun5/calculator/logic/Calculate.java
@@ -1,6 +1,5 @@
 package exam.seolheun5.calculator.logic;
 
-
 import exam.seolheun5.calculator.utils.Constants;
 
 import javax.swing.*;
@@ -17,17 +16,17 @@ import java.math.MathContext;
  * @lastModified 2024-10-27
  *
  * @changelog
- * <ul>
- *     <li>2024-10-16: 최초 생성</li>
- *     <li>2024-10-16: 더하기 계산 메서드 작성</li>
- *     <li>2024-10-16: 빼기, 곱하기, 나누기 계산 메서드 작 성</li>
- *     <li>2024-10-17: 첫 번째 숫자가 음수일 때 버그 수정</li>
- *     <li>2024-10-17: 0으로 나눌 때 오류 메시지 출력</li>
- *     <li>2024-10-18: 0으로 나눌 때 오류 메시지 폰트 오류 수정</li>
- *     <li>2024-10-19: 각 사칙연산 메서드에서 중복되는 내용을 하나의 메서드로 분리하여 호출</li>
- *     <li>2024-10-19: 피연산자 타입 BigDecimal로 변경</li>
- *     <li>2024-10-27: 피연산자 타입 수정 후 발생한 버그 수정</li>
- * </ul>
+ *            <ul>
+ *            <li>2024-10-16: 최초 생성</li>
+ *            <li>2024-10-16: 더하기 계산 메서드 작성</li>
+ *            <li>2024-10-16: 빼기, 곱하기, 나누기 계산 메서드 작 성</li>
+ *            <li>2024-10-17: 첫 번째 숫자가 음수일 때 버그 수정</li>
+ *            <li>2024-10-17: 0으로 나눌 때 오류 메시지 출력</li>
+ *            <li>2024-10-18: 0으로 나눌 때 오류 메시지 폰트 오류 수정</li>
+ *            <li>2024-10-19: 각 사칙연산 메서드에서 중복되는 내용을 하나의 메서드로 분리하여 호출</li>
+ *            <li>2024-10-19: 피연산자 타입 BigDecimal로 변경</li>
+ *            <li>2024-10-27: 피연산자 타입 수정 후 발생한 버그 수정</li>
+ *            </ul>
  */
 public class Calculate {
 
@@ -43,9 +42,9 @@ public class Calculate {
      * @create 2024-10-16
      * @lastModified 2024-10-16
      *
-     * @param resultLabel 결과 정보를 보여주는 {@code JLabel} 타입 변수
+     * @param resultLabel   결과 정보를 보여주는 {@code JLabel} 타입 변수
      * @param solutionLabel 풀이 과정을 보여주는 {@code JLabel} 타입 변수
-     * @param buttonText 버튼 텍스트 정보를 담고 있는 {@code String} 타입 변수
+     * @param buttonText    버튼 텍스트 정보를 담고 있는 {@code String} 타입 변수
      */
     public Calculate(JLabel resultLabel, JLabel solutionLabel, String buttonText) {
         this.resultLabel = resultLabel;
@@ -69,14 +68,16 @@ public class Calculate {
         BigDecimal firstNum;
         BigDecimal secondNum;
 
-        if(solutionText.startsWith(Constants.SUBTRACT)) {
+        if (solutionText.startsWith(Constants.SUBTRACT)) {
             int firstIndex = solutionText.indexOf(Constants.SUBTRACT);
 
             firstNum = new BigDecimal(solutionText.substring(0, (solutionText.indexOf(operator, firstIndex + 1))));
-            secondNum = new BigDecimal(solutionText.substring((solutionText.indexOf(operator, firstIndex + 1) + 1), solutionText.length() - 1));
+            secondNum = new BigDecimal(solutionText.substring((solutionText.indexOf(operator, firstIndex + 1) + 1),
+                    solutionText.length() - 1));
         } else {
             firstNum = new BigDecimal(solutionText.substring(0, (solutionText.indexOf(operator))));
-            secondNum = new BigDecimal(solutionText.substring((solutionText.indexOf(operator) + 1), solutionText.length() - 1));
+            secondNum = new BigDecimal(
+                    solutionText.substring((solutionText.indexOf(operator) + 1), solutionText.length() - 1));
         }
 
         String result = "";
@@ -106,7 +107,7 @@ public class Calculate {
                 break;
         }
 
-        if(result.substring(result.length() - 2).equals(".0")) {
+        if (result.length() > 2 && result.substring(result.length() - 2).equals(".0")) {
             result = result.substring(0, result.length() - 2);
         }
 
@@ -158,7 +159,7 @@ public class Calculate {
     public void calculateDivide() {
         String result = findResult(Constants.DIVIDE);
 
-        if(!result.equals("")) {
+        if (!result.equals("")) {
             resultLabel.setText(result);
         }
     }
